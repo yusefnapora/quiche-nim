11ad52d437ecc02754050c45fa803899a26d8bb7
Yusef Napora <yusef@napora.org>
2024-07-06 09:59:41 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+ outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        inherit (pkgs) lib;
+        inherit (pkgs.stdenv) isLinux;
+
+        rust-toolchain = pkgs.rust-bin.stable.latest.default;
+
+        packages-common = with pkgs; [
+          rust-toolchain
+          pkg-config
+          cmake
+          gnumake
+        ];
+
+        packages-linux = packages-common;
+
+        packages-darwin = packages-common ++ (with pkgs; [
+          libiconv
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.CoreServices
+          darwin.apple_sdk.frameworks.CoreFoundation
+          darwin.apple_sdk.frameworks.Foundation
+          darwin.apple_sdk.frameworks.AppKit
+          darwin.apple_sdk.frameworks.WebKit
+          darwin.apple_sdk.frameworks.Cocoa
+        ]);
+
+        packages = if isLinux then packages-linux else packages-darwin;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+        };
+      });
+}
