5ffe39d597d68445d05994ce4b88fdb8b70329bb
Yusef Napora <yusef@napora.org>
2024-07-21 11:05:46 -0400

    add wrappers for all quiche_config_* fns

--- quichenim/src/quichenim.nim
+++ quichenim/src/quichenim.nim
@@ -1,8 +1,16 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
+import std/sequtils
 import quichenim/ffi
+import quichenim/config
 
 when isMainModule:
   echo "quiche version: ", version()
+  var cfg = newQuicheConfig(QUICHE_PROTOCOL_VERSION)
+  echo "created a config object without blowing up", cfg
+  var res = cfg.set_application_protos("http/0.9")
+  echo "set app protos. result: ", res
 
+  cfg.set_cc_algorithm(enum_quiche_cc_algorithm.QUICHE_CC_RENO)
+  echo "set cc algo"
